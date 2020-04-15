7628ef6bc16209f5b20e84336fed4c334fe01aac
GalinaProkator <59877586+GalinaProkator@users.noreply.github.com>
2020-04-16 01:53:05 +0300

    Add files via upload

---
+++ PhraseCheck.java
@@ -0,0 +1,32 @@
+public class PhraseCheck {
+    public static void main(String[] args) throws Exception {
+        String string = "())";
+        if (isPhraseCorrect(string)){
+            System.out.println("The phrase is correct");
+        }
+    }
+
+    private static boolean isPhraseCorrect(String string) throws Exception {
+        char openingBrace = '(';
+        int count = 0;
+
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == openingBrace){
+                count++;
+            }
+            else{
+                count--;
+                if (count < 0){
+                    int braceNumber = i+1;
+                    throw new Exception("The phrase is incorrect. The brace # " + braceNumber + " is odd");
+                }
+            }
+        }
+        if (count > 0) {
+            throw new Exception("The phrase is incorrect. There are " + count + " unclosed opening braces");
+        }
+        else {
+            return true;
+        }
+    }
+}
